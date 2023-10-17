7824ace54b422cf1e8d386f24a3bd61a488bec34
Povel Forsare Källman <biopovel@hotmail.com>
2023-10-17 15:14:29 +0200

    run

--- notebook/web/fetch_abstract.py
+++ notebook/web/fetch_abstract.py
@@ -1,14 +1,13 @@
 # filename: fetch_abstract.py
 
-import urllib.request
-import re
+import requests
+from bs4 import BeautifulSoup
 
 def fetch_abstract(url):
-    with urllib.request.urlopen(url) as response:
-        html = response.read().decode()
-        abstract = re.search('<blockquote class="abstract mathjax">(.*?)</blockquote>', html, re.DOTALL).group(1)
-        abstract = re.sub('<[^<]+?>', '', abstract)  # Remove HTML tags
-        return abstract.strip()
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    abstract = soup.find('blockquote', attrs={'class': 'abstract mathjax'}).text.strip()
+    return abstract
 
 url = "https://arxiv.org/abs/2308.08155"
 print(fetch_abstract(url))
